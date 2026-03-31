a8d55137d4193f07a370d3a2bf42b141d7f14a31
Henrique Leal <henriquelealsouza671@gmail.com>
2026-03-31 03:07:38 -0300

    Fix: Atualizando Node.js para versão 22 para compatibilidade com Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.playcineverse.app',
+  appName: 'Cineverse',
+  webDir: 'dist'
+};
+
+export default config;
